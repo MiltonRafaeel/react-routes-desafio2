f193540e094b114985690051c21b5f5b6aef8487
MiltonRafael <miltonrafael166@gmail.com>
2026-05-14 18:55:50 -0300

    Testing welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import ContentMain from './components/ContentMain'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Teste</h1>
+      <Header />
+      <ContentMain />
     </>
   )
 }
---
+++ src/components/ContentMain/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export default function ContentMain() {
+
+    return (
+        <section className="title-welcome-container">
+            <h2 className="title-welcome">Bem-vindos!</h2>
+        </section>
+    );
+}
